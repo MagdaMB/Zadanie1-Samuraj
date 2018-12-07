389a3d6cf357d5f03e052beacbf334cb0d9acbe9
MagdaMB <magda.m.badura@gmail.com>
2018-12-07 20:32:14 +0100

    new

--- js/script.js
+++ js/script.js
@@ -1,20 +1,8 @@
 const clickButton = document.querySelector(".clickBtn");
 let numberOfNewElement = 0;
 
-// clickButton.addEventListener('click', () => {
+clickButton.addEventListener('click', () => {
 
-//     const newElement = document.createElement("div");
-//     newElement.textContent = ++numberOfNewElement;
-
-//     document.body.appendChild(newElement);
-
-//     if (numberOfNewElement % 5 === 0) {
-//         newElement.classList.add("circleBox");
-//     }
-
-// })
-
-const addElement = function () {
     const newElement = document.createElement("div");
     newElement.textContent = ++numberOfNewElement;
 
@@ -23,5 +11,17 @@ const addElement = function () {
     if (numberOfNewElement % 5 === 0) {
         newElement.classList.add("circleBox");
     }
-}
-clickButton.addEventListener('click', addElement);
+
+})
+
+// const addElement = function () {
+//     const newElement = document.createElement("div");
+//     newElement.textContent = ++numberOfNewElement;
+
+//     document.body.appendChild(newElement);
+
+//     if (numberOfNewElement % 5 === 0) {
+//         newElement.classList.add("circleBox");
+//     }
+// }
+// clickButton.addEventListener('click', addElement);
